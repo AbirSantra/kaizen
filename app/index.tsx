cf72d252359812d0f7fce5d947fa88fb1b510cfb
Abir Santra <santraabir8@gmail.com>
2026-05-02 11:56:23 +0530

    feat: add clsx and tailwind-merge utility

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
+import { cn } from "@/lib/utils";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-white dark:bg-slate-900">
+      <Text className={cn("text-2xl font-bold text-gray-900 dark:text-white")}>
         Welcome to Kaizen!
       </Text>
     </View>
